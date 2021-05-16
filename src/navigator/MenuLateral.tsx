f7325963653364c47a9f04f931bec698c71e3f97
CristianX <thecristianx@hotmail.com>
2021-05-16 00:01:55 -0500

    Añadiendo opciones a menú lateral Drawer

--- src/navigator/MenuLateral.tsx
+++ src/navigator/MenuLateral.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Image, Text, useWindowDimensions, View } from 'react-native';
+import { Image, Text, useWindowDimensions, View, TouchableOpacity } from 'react-native';
 import { createDrawerNavigator, DrawerContentComponentProps, DrawerContentOptions, DrawerContentScrollView } from '@react-navigation/drawer';
 import { StackNavigator } from './StackNavigator';
 import { SettingsScreens } from '../screens/SettingsScreens';
 import { styles } from '../theme/appTheme';
+
 
 const Drawer = createDrawerNavigator();
 
@@ -20,17 +21,33 @@ export const  MenuLateral = () => {
   );
 }
 
-const MenuInterno = ( props:  DrawerContentComponentProps<DrawerContentOptions>) => {
+const MenuInterno = ( {navigation} :  DrawerContentComponentProps<DrawerContentOptions>) => {
 
   return (
 
     <DrawerContentScrollView>
+
+      {/* Parte del avatar */}
       <View style={ styles.avatarContainer }>
         <Image 
           source={{uri: 'https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png'}} 
           style={ styles.avatar }
         />
       </View>
+
+      {/* Opciones de meú */}
+      <View style={styles.menuContainer}>
+
+        <TouchableOpacity style={styles.menuBoton } onPress={ () => navigation.navigate('StackNavigator') } >
+          <Text style={ styles.menuTexto }>Navegación</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.menuBoton } onPress={ () => navigation.navigate('SettingsScreens') } >
+          <Text style={ styles.menuTexto }>Ajustes</Text>
+        </TouchableOpacity>
+
+      </View>
+
     </DrawerContentScrollView>
 
   );
