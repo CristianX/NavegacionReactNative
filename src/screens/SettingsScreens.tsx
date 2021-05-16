e116587dfcec7144f7d9e1e598046c3c881aa98f
CristianX <thecristianx@hotmail.com>
2021-05-15 21:59:47 -0500

    Implementando Drawer Navigation para menú lateral

---
+++ src/screens/SettingsScreens.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const SettingsScreens = () => {
+    return (
+        <View>
+            <Text>Settings Screen</Text>
+        </View>
+    )
+}
