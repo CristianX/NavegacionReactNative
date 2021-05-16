a55d50364025e26c5b820a6d0274891909c9a8ed
CristianX <thecristianx@hotmail.com>
2021-05-15 19:32:43 -0500

    Añadiendo pre requisitos para uso de navigation stack

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { Text, View } from 'react-native';
 
+import { NavigationContainer } from '@react-navigation/native';
+
 const App = () => {
   return (
-    <View>
+    <NavigationContainer>
       <Text>Hola Mundo</Text>
-    </View>
+    </NavigationContainer>
   )
 }
 
