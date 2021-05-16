f7e6ce35a3ba0d3357fc3291e8da2af318272361
CristianX <thecristianx@hotmail.com>
2021-05-15 19:50:09 -0500

    Añadiendo componente de navegación Stacknavigator e implementandoló en App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, View } from 'react-native';
+
 
 import { NavigationContainer } from '@react-navigation/native';
+import { StackNavigator } from './src/navigator/StackNavigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Text>Hola Mundo</Text>
+      <StackNavigator/>
     </NavigationContainer>
   )
 }
