8d2fa29559289941cfa040add9da59912e7cb6c0
CristianX <thecristianx@hotmail.com>
2021-05-16 22:29:44 -0500

    Cambiando el estado inicial para disparar acciones

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -33,11 +33,16 @@ export const AuthProvider = ({ children }: any ) => {
 
 
 const [authState, dispatch] = useReducer(authReducer, authInitialState);
+
+const signIn = () => {
+    dispatch({ type: 'signIn'});
+}
+
     
     return (
         <AuthContext.Provider value={{
             authState,
-            signIn: () => {}
+            signIn,
         }}>
             { children }
         </AuthContext.Provider>
--- src/context/authReducer.tsx
+++ src/context/authReducer.tsx
@@ -1,11 +1,22 @@
 import { AuthState } from './AuthContext';
 
 
+type AuthAction = { type: 'signIn' };
 
+// generaEstado
+export const authReducer = ( state: AuthState, action: AuthAction ): AuthState => {  //Siempre ser de tipado del estado inicial
 
-
-export const authReducer = ( state: AuthState, action: any ): AuthState => {  //Siempre ser de tipado del estado inicial
-
-    return state;
+    switch ( action.type ) {
+        case 'signIn':
+            // state.isLoggedIn = true nunca se debe hacer esto por que se esta mutando el estado por lo que react no lo captara de buena maneraq
+            return {
+                ...state,
+                isLoggedIn: true,
+                userName: 'no-username-yet'
+            }
+    
+        default:
+            return state;
+    }
 
 }
