ee06cf21bf49b7fc661cb6b9e68ac20e9f9fcb49
CristianX <thecristianx@hotmail.com>
2021-05-16 19:44:25 -0500

    Añadiendo paquete de íconos pde IonIcons y realizando la configurtación para android

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,15 +1,22 @@
 import React, { useEffect } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme';
+
+
 
 export const Tab1Screen = () => {
-
+    
     useEffect(() => {
         console.log('Tab1Screen Use Effect');
     }, [])
-
+    
     return (
-        <View>
-            <Text>Tab 1 Screen</Text>
+        <View style={ styles.globalMargin } >
+            <Text style={ styles.title } >Iconos</Text>
+            <Text>
+                <Icon name="airplane-outline" size={50} color="#900" />
+            </Text>
         </View>
     )
 }
