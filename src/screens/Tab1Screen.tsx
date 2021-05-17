1da63b5ab62fad39961beaca677e79b1e6992527
CristianX <thecristianx@hotmail.com>
2021-05-16 20:00:22 -0500

    Mostrando íconos en la pantalla de tab 1

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -1,21 +1,33 @@
 import React, { useEffect } from 'react'
 import Icon from 'react-native-vector-icons/Ionicons';
 import { Text, View } from 'react-native'
-import { styles } from '../theme/appTheme';
+import { styles, colores } from '../theme/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 
 
 export const Tab1Screen = () => {
     
+    const { top } = useSafeAreaInsets();
+
     useEffect(() => {
         console.log('Tab1Screen Use Effect');
     }, [])
     
     return (
-        <View style={ styles.globalMargin } >
+        <View style={{
+            ...styles.globalMargin,
+            marginTop: top + 10
+        }}>
             <Text style={ styles.title } >Iconos</Text>
             <Text>
-                <Icon name="airplane-outline" size={50} color="#900" />
+                <Icon name="airplane-outline" size={50} color={ colores.primary } />
+                <Icon name="attach-outline" size={50} color={ colores.primary } />
+                <Icon name="barbell-outline" size={50} color={ colores.primary } />
+                <Icon name="bicycle-outline" size={50} color={ colores.primary } />
+                <Icon name="boat-outline" size={50} color={ colores.primary } />
+                <Icon name="bulb-outline" size={50} color={ colores.primary } />
+                <Icon name="car-sport-outline" size={50} color={ colores.primary } />
             </Text>
         </View>
     )
