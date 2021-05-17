a8b428753f775a79d037f4656eb92196204ce813
CristianX <thecristianx@hotmail.com>
2021-05-16 22:55:30 -0500

    Añadiendo un custom component para obtener un touchableOpacity personalizado

--- src/screens/Tab1Screen.tsx
+++ src/screens/Tab1Screen.tsx
@@ -3,6 +3,7 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { Text, View } from 'react-native'
 import { styles, colores } from '../theme/appTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { TouchableIcon } from '../components/TouchableIcon';
 
 
 
@@ -21,13 +22,13 @@ export const Tab1Screen = () => {
         }}>
             <Text style={ styles.title } >Iconos</Text>
             <Text>
-                <Icon name="airplane-outline" size={80} color={ colores.primary } />
-                <Icon name="attach-outline" size={80} color={ colores.primary } />
-                <Icon name="barbell-outline" size={80} color={ colores.primary } />
-                <Icon name="bicycle-outline" size={80} color={ colores.primary } />
-                <Icon name="boat-outline" size={80} color={ colores.primary } />
-                <Icon name="bulb-outline" size={80} color={ colores.primary } />
-                <Icon name="car-sport-outline" size={80} color={ colores.primary } />
+                <TouchableIcon iconName="airplane-outline"  />
+                <TouchableIcon iconName="attach-outline"  />
+                <TouchableIcon iconName="barbell-outline"  />
+                <TouchableIcon iconName="bicycle-outline"  />
+                <TouchableIcon iconName="boat-outline"  />
+                <TouchableIcon iconName="bulb-outline"  />
+                <TouchableIcon iconName="car-sport-outline"  />
             </Text>
         </View>
     )
