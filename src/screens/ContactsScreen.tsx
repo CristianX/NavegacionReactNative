210aaab160c98bff220db2ff0dd9228f74b6fb46
CristianX <thecristianx@hotmail.com>
2021-05-16 22:47:26 -0500

    Cambiando de estado al usar el boton SignIn de la pantalla de contactos

--- src/screens/ContactsScreen.tsx
+++ src/screens/ContactsScreen.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React, { useContext } from 'react'
+import { Button, Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+import { AuthContext } from '../context/AuthContext';
 
 export const ContactsScreen = () => {
+
+    const { signIn, authState: { isLoggedIn } } = useContext( AuthContext )
+
     return (
-        <View>
-            <Text>Contacts Screen</Text>
+        <View style={ styles.globalMargin }>
+            <Text style={ styles.title }>Contacts Screen</Text>
+
+            {
+                ( !isLoggedIn ) && ( <Button title="SignIn" onPress={  signIn } ></Button> )
+            }
+
+            
+
         </View>
     )
 }
